53c88a7f324c3d07e3394f37ab77e539ab01dfa5
Chmouel Boudjnah <chmouel@chmouel.com>
2018-11-16 10:50:58 +0100

    Make sure name is UNIQUE

--- migration/sql-files/001-pipelineenv.sql
+++ migration/sql-files/001-pipelineenv.sql
@@ -5,7 +5,7 @@ CREATE TABLE pipelines (
     updated_at timestamp with time zone,
     deleted_at timestamp with time zone,
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    name text NOT NULL,
+    name text UNIQUE NOT NULL,
     space_id uuid,
     PRIMARY KEY(id)
 );
