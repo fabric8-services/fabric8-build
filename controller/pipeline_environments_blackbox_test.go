6aec358a77ab7834431761f06beef2e77897ab39
Chmouel Boudjnah <chmouel@chmouel.com>
2018-11-16 10:58:05 +0100

    Improve controller tests by handling the Failure cases
    Improve controller tests with the failure cases,
    
    We can't use goa builtin tests automatic because it only handle the sucess cases
    and we won't be able to catch the error as a 'good' failure.
    
    We then build our own and handle the failures there.

--- controller/pipeline_environments_blackbox_test.go
+++ controller/pipeline_environments_blackbox_test.go
@@ -2,6 +2,11 @@ package controller_test
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/fabric8-services/fabric8-build/app"
@@ -43,6 +48,14 @@ func (s *PipelineEnvironmentControllerSuite) SetupSuite() {
 
 	s.db = gormapp.NewGormDB(s.DB)
 
+	//TODO(chmouel): really need something better
+	for _, table := range []string{"environments", "pipelines"} {
+		_, err := s.DB.DB().Exec("DELETE FROM " + table + " CASCADE")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	// TODO(chmouel): change this when we have jwt support,
 	svc := testauth.UnsecuredService("ppl-test1")
 	s.svc = svc
@@ -50,22 +63,49 @@ func (s *PipelineEnvironmentControllerSuite) SetupSuite() {
 	s.ctrl = controller.NewPipelineEnvironmentController(s.svc, s.db)
 }
 
+// createPipelineEnvironmentCtrlNoErroring we do this one manually cause the one from
+// goatest one exit on errro without being able to catch
+func (s *PipelineEnvironmentControllerSuite) createPipelineEnvironmentCtrlNoErroring() (*app.CreatePipelineEnvironmentsContext, *httptest.ResponseRecorder) {
+	spaceID := uuid.NewV4()
+	rw := httptest.NewRecorder()
+	u := &url.URL{
+		Path: fmt.Sprintf("/api/pipelines/environments/%v", spaceID),
+	}
+	req, _err := http.NewRequest("POST", u.String(), nil)
+	if _err != nil {
+		panic("invalid test " + _err.Error()) // bug
+	}
+	prms := url.Values{}
+	prms["spaceID"] = []string{fmt.Sprintf("%v", spaceID)}
+	goaCtx := goa.NewContext(goa.WithAction(s.ctx, "PipelineEnvironmentsTest"), rw, req, prms)
+	createCtx, __err := app.NewCreatePipelineEnvironmentsContext(goaCtx, req, s.svc)
+	if __err != nil {
+		panic("invalid test data " + __err.Error()) // bug
+	}
+	return createCtx, rw
+}
+
 func (s *PipelineEnvironmentControllerSuite) TestCreate() {
 	s.T().Run("ok", func(t *testing.T) {
 		spaceID := uuid.NewV4()
 		envID := uuid.NewV4()
+
 		payload := newPipelineEnvironmentPayload("osio-stage", envID)
-
 		_, newEnv := test.CreatePipelineEnvironmentsCreated(t, s.ctx, s.svc, s.ctrl, spaceID, payload)
-
 		assert.NotNil(t, newEnv)
 		assert.NotNil(t, newEnv.Data.ID)
 		assert.NotNil(t, newEnv.Data.Environments[0].EnvUUID)
 
-		// TODO(chmouel): add this when we have show controller
-		// _, env := test.ShowPipelineEnvironmentsOK(t, s.ctx, s.svc, s.ctrl, *newEnv.Data.ID)
-		// require.NotNil(t, env)
-		// assert.Equal(t, env.Data.ID, newEnv.Data.ID)
+		createCtxerr, rw := s.createPipelineEnvironmentCtrlNoErroring()
+		createCtxerr.Payload = payload
+		s.ctrl.Create(createCtxerr)
+		require.Equal(s.T(), 500, rw.Code)
+
+		createCtxerr, rw = s.createPipelineEnvironmentCtrlNoErroring()
+		emptyPayload := &app.CreatePipelineEnvironmentsPayload{}
+		createCtxerr.Payload = emptyPayload
+		s.ctrl.Create(createCtxerr)
+		require.Equal(s.T(), 400, rw.Code)
 	})
 
 }
