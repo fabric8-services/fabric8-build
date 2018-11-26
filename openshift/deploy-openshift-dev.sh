6911e69708cacbf1c1f271dee5e92693dc1e20fe
Chmouel Boudjnah <chmouel@chmouel.com>
2018-11-26 13:43:47 +0100

    Use only one DB when deploying with deploy-openshift-dev
    Save resources save the cheerleader save the world!
    
    This closes openshiftio/openshift.io#4586 and closes #119

--- openshift/deploy-openshift-dev.sh
+++ openshift/deploy-openshift-dev.sh
@@ -18,23 +18,49 @@ eval $(make print-env|egrep '^(REGISTRY_URL|AUTH|DB|ENV).*(IMAGE|PORT|NAME)')
 oc whoami 2>/dev/null >/dev/null || { echo "oc does not seem to be configured properly"; exit 1 ;}
 
 SERVER=$(oc project|sed 's/.*on server.*https:\/\///;s/:.*//')
-
 FORCE_DELETE_VARS="--force --wait=true --grace-period=0"
+DC_DB=db
 
 IMAGE_PULL_POLICY="IfNotPresent"
 [[ ${SERVER} =~ .*(devshift.net|openshift.com) ]] && {
     IMAGE_PULL_POLICY="Always"
 }
 
+function waitForDC() {
+    DC=$1
+    local max=60 # 2mn
+    local cnt=0
+
+    while [[ $(oc get dc/${DC} -o json | jq '.status.availableReplicas') < 1 ]];do
+        [[ ${cnt} > ${max} ]] && {
+            echo "Timing out while waiting for DC/${DC}";
+            exit 1
+        }
+
+        sleep 2
+        cnt+=1
+    done
+}
+
 function deploy_db() {
+    DBS=${1}
     POSTGRESQL_ADMIN_PASSWORD=`sed -n '/postgres.password/ { s/.*: //;p ;}' config.yaml`
-    oc new-app --name=${1} -e \
+    oc new-app --name=${DC_DB} -e \
        POSTGRESQL_ADMIN_PASSWORD=${POSTGRESQL_ADMIN_PASSWORD} \
        ${DB_CONTAINER_IMAGE} -o yaml | oc delete ${FORCE_DELETE_VARS} -f- 2>/dev/null || true
     sleep 2
-    oc new-app --name=${1} -e \
+    oc new-app --name=${DC_DB} -e \
        POSTGRESQL_ADMIN_PASSWORD=${POSTGRESQL_ADMIN_PASSWORD} \
        ${DB_CONTAINER_IMAGE}
+
+    waitForDC ${DC_DB}
+
+    sleep 2
+
+    for x in ${DBS};do
+        oc rsh dc/${DC_DB} psql -c  "create database ${x};"
+    done
+
 }
 
 function deploy_sideservice() {
@@ -69,6 +95,8 @@ function deploy_app() {
     # do this after applying the configs
     oc set env dc/f8build \
        F8_LOG_LEVEL=debug \
+       F8_POSTGRES_HOST=${DC_DB} \
+       F8_POSTGRES_DATABASE=build \
        F8_DEVELOPER_MODE_ENABLED=1 \
        F8_ENVIRONMENT=local F8_POSTGRES_SSLMODE=disable \
        F8_AUTH_URL="http://${AUTH_CONTAINER_NAME}:${AUTH_CONTAINER_PORT}"
@@ -77,16 +105,13 @@ function deploy_app() {
     oc expose service/f8build
 }
 
-# TODO(chmouel): one db for all
-# DBS
-deploy_db db
-deploy_db ${AUTH_DB_CONTAINER_NAME}
-deploy_db ${ENV_DB_CONTAINER_NAME}
+deploy_db "build auth env"
 
 # AUTH
 AUTH_SERVICE_VARIABLES=$(cat <<EOF
 -e AUTH_LOG_LEVEL=debug
--e AUTH_POSTGRES_HOST=${AUTH_DB_CONTAINER_NAME}
+-e AUTH_POSTGRES_HOST=${DC_DB}
+-e AUTH_POSTGRES_DATABASE=auth
 -e AUTH_POSTGRES_PORT=5432
 -e AUTH_DEVELOPER_MODE_ENABLED=true
 EOF
@@ -96,13 +121,15 @@ deploy_sideservice ${AUTH_CONTAINER_NAME} ${AUTH_CONTAINER_IMAGE} "${AUTH_SERVIC
 # ENV
 ENV_SERVICE_VARIABLES=$(cat <<EOF
 -e F8_LOG_LEVEL=debug
--e F8_POSTGRES_HOST=${ENV_DB_CONTAINER_NAME}
+-e F8_POSTGRES_HOST=${DC_DB}
+-e F8_POSTGRES_DATABASE=env
 -e F8_POSTGRES_PORT=5432
 -e F8_DEVELOPER_MODE_ENABLED=true
 -e F8_AUTH_URL=http://${AUTH_CONTAINER_NAME}:${AUTH_CONTAINER_PORT}
 EOF
 )
 deploy_sideservice ${ENV_CONTAINER_NAME} ${ENV_CONTAINER_IMAGE} "${ENV_SERVICE_VARIABLES}"
+
 
 # Build
 deploy_app
